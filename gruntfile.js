41340fb15014bf92847c9d6903ebe0576669dd6b
Dennis Yang <sinned@gmail.com>
2016-12-16 15:38:17 -0800

    checking in working bot.

--- gruntfile.js
+++ gruntfile.js
@@ -48,7 +48,7 @@ module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
 
   // Making grunt default to force in order not to break the project.
-  //grunt.option('force', true);
+  grunt.option('force', true);
 
   // Default task(s).
   grunt.registerTask('default', ['jshint', 'concurrent:default']);
